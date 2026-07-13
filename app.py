9f0e13e9163346c70da88cae67d8cc4ee44fc6d0
Chad <chadflowers99@gmail.com>
2026-07-13 18:48:23 -0500

    Change CASH FLOW label to FLEX BUDGET

--- app.py
+++ app.py
@@ -232,7 +232,7 @@ def auth_ui():
         <div style="text-align: center;">
             <h1>Portfolio brand.</h1>
             <div style="font-size: 18px; margin-bottom: 30px;">
-                CASH FLOW
+                FLEX BUDGET
             </div>
         </div>
         """,
